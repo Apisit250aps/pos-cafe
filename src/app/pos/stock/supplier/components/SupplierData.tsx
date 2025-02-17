7f302deaefd4c1ffa9b5b2803a58c9af4d66a79a
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2025-02-17 22:29:01 +0700

    supplier store with table

--- src/app/pos/stock/supplier/components/SupplierData.tsx
+++ src/app/pos/stock/supplier/components/SupplierData.tsx
@@ -1,17 +1,13 @@
 'use client';
-import { ISupplier } from '@/models/suppliers';
-import { fetchSupplier } from '@/services/supplier';
-import { useCallback, useEffect, useState } from 'react';
+
+import useSupplier from '@/stores/useSupplier';
+import { useEffect } from 'react';
 
 export default function SupplierData() {
-  const [suppliers, setSupplierData] = useState<ISupplier[]>([]);
-  const fetchData = useCallback(async () => {
-    const { data } = await fetchSupplier({});
-    setSupplierData(data!);
-  }, []);
+  const { suppliers, loadSuppliers, loading } = useSupplier();
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    loadSuppliers();
+  }, [loadSuppliers]);
   return (
     <div className="overflow-x-auto min-h-96">
       <table className="table">
@@ -27,37 +23,65 @@ export default function SupplierData() {
           </tr>
         </thead>
         <tbody>
-          {suppliers.map((supplier, index) => (
-            <tr key={index} className="hover">
-              <th>{index + 1}</th>
-              <td>{supplier.name}</td>
-              <td>{supplier.contract_person}</td>
-              <td>{supplier.phone}</td>
-              <td>{supplier.email}</td>
-              <td>
-                <div className="dropdown dropdown-end">
-                  <div tabIndex={0} role="button" className="btn btn-sm m-1">
-                    <i className="bx bx-dots-vertical-rounded"></i>
-                  </div>
-                  <ul
-                    tabIndex={0}
-                    className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-                  >
-                    <li>
-                      <a href={`/pos/stock/supplier/${supplier._id}`}>
-                        <i className="bx bx-pencil"></i>Edit
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <i className="bx bx-trash"></i>Delete
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </td>
-            </tr>
-          ))}
+          {loading ? (
+            <>
+              <tr>
+                <td colSpan={6} className="text-center">
+                  <span className="loading loading-dots loading-sm"></span>
+                </td>
+              </tr>
+            </>
+          ) : (
+            <>
+              {!suppliers.length ? (
+                <>
+                  <tr>
+                    <td colSpan={6} className="text-center">
+                      No suppliers found.
+                    </td>
+                  </tr>
+                </>
+              ) : (
+                <>
+                  {suppliers.map((supplier, index) => (
+                    <tr key={index} className="hover">
+                      <th>{index + 1}</th>
+                      <td>{supplier.name}</td>
+                      <td>{supplier.contract_person}</td>
+                      <td>{supplier.phone}</td>
+                      <td>{supplier.email}</td>
+                      <td>
+                        <div className="dropdown dropdown-end">
+                          <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-sm m-1"
+                          >
+                            <i className="bx bx-dots-vertical-rounded"></i>
+                          </div>
+                          <ul
+                            tabIndex={0}
+                            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+                          >
+                            <li>
+                              <a href={`/pos/stock/supplier/${supplier._id}`}>
+                                <i className="bx bx-pencil"></i>Edit
+                              </a>
+                            </li>
+                            <li>
+                              <a>
+                                <i className="bx bx-trash"></i>Delete
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </>
+              )}
+            </>
+          )}
         </tbody>
       </table>
     </div>
